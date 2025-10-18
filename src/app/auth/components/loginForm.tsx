dc1d7ab0d501875af065be284dc32b6b54a64b30
Om Bikram Rana <ome.bikram@gmail.com>
2025-10-18 08:54:54 +0200

    build setup_staging

--- src/app/auth/components/loginForm.tsx
+++ src/app/auth/components/loginForm.tsx
@@ -9,7 +9,7 @@ export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
-   const [error, setError] = useState("");
+  const [error, setError] = useState("");
   const router = useRouter();
 
   const users = [
--- src/app/dashboard/pages/eventdetails/[id]/page.tsx
+++ src/app/dashboard/pages/eventdetails/[id]/page.tsx
@@ -1,15 +1,35 @@
-"use client"
-import { useParams } from "next/navigation";
+// "use client"
+// import { useParams } from "next/navigation";
+// import EventDetails from "@/app/dashboard/components/eventDetails";
+// import { events } from "@/app/data/events";
+
+
+// export default function EventDetailsPage(){
+// const params = useParams();
+// const event = events.find(e => e.id === params.id );
+
+// if(!event){
+//     return <div className="p-6 text-red-600">Event not found!</div>
+// }
+//     return <EventDetails event={event}/>;
+// }
+
 import EventDetails from "@/app/dashboard/components/eventDetails";
 import { events } from "@/app/data/events";
 
-
-export default function EventDetailsPage(){
-const params = useParams();
-const event = events.find(e => e.id === params.id );
-
-if(!event){
-    return <div className="p-6 text-red-600">Event not found!</div>
+export function generateStaticParams() {
+  return events.map((event) => ({
+    id: event.id.toString(),
+  }));
 }
-    return <EventDetails event={event}/>;
+
+export default async function EventDetailsPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params; 
+  const event = events.find((e) => e.id === id);
+
+  if (!event) {
+    return <div className="p-6 text-red-600">Event not found!</div>;
+  }
+
+  return <EventDetails event={event} />;
 }
