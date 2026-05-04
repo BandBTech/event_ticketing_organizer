8fd535470caa05f54af363d1c973257a97e7f98b
KosalRaj <koshalshakya72@gmail.com>
2026-05-04 21:28:32 +0545

    Adjust max-width for payout details page

--- src/pages/organizerDashboard/payouts/detail.tsx
+++ src/pages/organizerDashboard/payouts/detail.tsx
@@ -191,13 +191,11 @@ export default function PayoutDetailPage() {
   return (
     <>
       <Head>
-        <title>
-          {t("payouts.payoutDetails", "Payout Details")}
-        </title>
+        <title>{t("payouts.payoutDetails", "Payout Details")}</title>
       </Head>
       <DashboardLayout>
         <ProtectedRoute>
-          <div className="flex-1 max-w-4xl mx-auto p-4 md:p-6 space-y-5">
+          <div className="flex-1 max-w-7xl mx-auto p-4 md:p-6 space-y-5">
             {/* Back nav */}
             <button
               onClick={() => router.push("/organizerDashboard/payouts")}
